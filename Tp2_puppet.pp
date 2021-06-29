d96bba0163523a357c8685ada3d568d910813a18
islem <islemmanai4@gmail.com>
2021-06-29 14:26:29 +0100

    tp2 commit

--- Tp2_puppet.pp
+++ Tp2_puppet.pp
@@ -1,67 +1,67 @@
+$path1 = '/usr/src'
+$path2 = '/usr/bin'
+$path3 = '/var/www'
+
 class dokuwiki {
-    package {
-        'apache2':
+    package { 'apache2':
             ensure => present
     }
-    package {
-        'php7.3':
+
+    package { 'php7.3':
             ensure => present
     }
-    file {
-        'download dokuwiki':
-            path => '/usr/src/dokuwiki.tgz',
+	
+    file { 'download-dokuwiki':
             ensure => present,
-            source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz'
+            source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+            path   => "${path1}/dokuwiki.tgz"
     }
-    exec {
-        'extract dokuwiki':
+
+    exec { 'extract-dokuwiki':
             command => 'tar xavf dokuwiki.tgz',
-            cwd => '/usr/src',
-            path => ['/usr/bin'],
-            require => File['download dokuwiki'],
-            unless => 'test -d /usr/src/dokuwiki-2020-07-29/'
+            cwd     => "${path1}",
+            path    => ["${path2}"],
+            require => File['download-dokuwiki'],
+            unless  => "test -d ${path1}/dokuwiki-2020-07-29"
     }
-    file {
-        'rename dokuwiki':
-            path => '/usr/src/dokuwiki',
-            ensure => present,
-            source => '/usr/src/dokuwiki-2020-07-29',
-            require => Exec['extract dokuwiki']
+
+    file { 'rename-dokuwiki-2020-07-29':
+            ensure  => present,
+            source  => "${path1}/dokuwiki-2020-07-29",
+            path    => "${path1}/dokuwiki",
+            require => Exec['extract-dokuwiki']
     }
-    file {
-        'delete extracted dokuwiki':
-            path => '/usr/src/dokuwiki-2020-07-29',
-            ensure => absent,
-            require => File['rename dokuwiki']
-    }
-    #file {
-    #    'delete archive':
-    #        path => '/usr/src/dokuwiki.tgz',
-    #        ensure => absent
-    #}
 }
 
-node server0 {
+class dokuwikirecettes {
+    file { 'create new directory for recettes.wiki in /var/www and allow apache to write in':
+            ensure  => directory,
+            source  => "${path1}/dokuwiki",
+            path    => "${path3}/recettes.wiki",
+            recurse => true,
+            owner   => 'www-data',
+            group   => 'www-data',
+            require => File['rename-dokuwiki-2020-07-29']
+    }
+}
+
+class dokuwikipolitique {
+    file { 'create new directory for politique.wiki in /var/www and allow apache to write in':            ensure  => directory,
+            source  => "${path1}/dokuwiki",
+            path    => "${path3}/politique.wiki",
+            recurse => true,
+            owner   => 'www-data',
+            group   => 'www-data',
+            require => File['rename-dokuwiki-2020-07-29']
+    }
+}
+
+node 'server0' {
     include dokuwiki
+    include dokuwikirecettes
 }
 
-# file {
-#     'create recettes.wiki directory':
-#         ensure  => directory,
-#         path    => '/var/www/recettes.wiki',
-#         source  => '/usr/src/dokuwiki',
-#         recurse => true,
-#         owner   => 'www-data',
-#         group   => 'www-data',
-#         require => File['rename dokuwiki']
-# }
-# file {
-#     'create politique.wiki directory':
-#         ensure  => directory,
-#         path    => '/var/www/politique.wiki',
-#         source  => '/usr/src/dokuwiki',
-#         recurse => true,
-#         owner   => 'www-data',
-#         group   => 'www-data',
-#         require => File['rename dokuwiki']
-# }
+node 'server1' {
+    include dokuwiki
+    include dokuwikipolitique
+}
