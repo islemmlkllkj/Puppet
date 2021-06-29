cbcb5cc2e763abe3c63808d97238397a6cb15c3c
islem <islemmanai4@gmail.com>
2021-06-29 15:48:12 +0100

    first commit

--- Tp2_puppet.pp
+++ Tp2_puppet.pp
@@ -1,8 +1,8 @@
-$pathsrc = '/usr/src'
-$pathbin = '/usr/bin'
-$pathweb = '/var/www'
-
 class dokuwiki {
+    $path_src = '/usr/src'
+    $path_bin = '/usr/bin'
+    $path_web = '/var/www'
+
     package { 'apache2':
             ensure => present
     }
@@ -10,45 +10,37 @@ class dokuwiki {
     package { 'php7.3':
             ensure => present
     }
-	
+
     file { 'download-dokuwiki':
             ensure => present,
             source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
-            path   => "${pathsrc}/dokuwiki.tgz"
+            path   => "${path_src}/dokuwiki.tgz"
     }
 
     exec { 'extract-dokuwiki':
             command => 'tar xavf dokuwiki.tgz',
-            cwd     => "${pathsrc}",
-            path    => ["${pathbin}"],
+            cwd     => "${path_src}",
+            path    => ["${path_bin}"],
             require => File['download-dokuwiki'],
-            unless  => "test -d ${pathsrc}/dokuwiki-2020-07-29"
+            unless  => "test -d ${path_src}/dokuwiki-2020-07-29"
     }
 
     file { 'rename-dokuwiki-2020-07-29':
             ensure  => present,
-            source  => "${pathsrc}/dokuwiki-2020-07-29",
-            path    => "${pathsrc}/dokuwiki",
+            source  => "${path_src}/dokuwiki-2020-07-29",
+            path    => "${path_src}/dokuwiki",
             require => Exec['extract-dokuwiki']
     }
 }
 
-class dokuwikirecettes {
-    file { 'create new directory for recettes.wiki in /var/www and allow apache to write in':
+class dokuwiki_deploy {
+    $path_src = '/usr/src'
+    $path_web = '/var/www'
+	
+    file { "create new directory for ${env}.wiki in ${path_web} and allow apache to write in":            
             ensure  => directory,
-            source  => "${pathsrc}/dokuwiki",
-            path    => "${pathweb}/recettes.wiki",
-            recurse => true,
-            owner   => 'www-data',
-            group   => 'www-data',
-            require => File['rename-dokuwiki-2020-07-29']
-    }
-}
-
-class dokuwikipolitique {
-    file { 'create new directory for politique.wiki in /var/www and allow apache to write in':            ensure  => directory,
-            source  => "${pathsrc}/dokuwiki",
-            path    => "${pathweb}/politique.wiki",
+            source  => "${path_src}/dokuwiki",
+            path    => "${path_web}/${env}.wiki",
             recurse => true,
             owner   => 'www-data',
             group   => 'www-data',
@@ -57,11 +49,24 @@ class dokuwikipolitique {
 }
 
 node 'server0' {
+    #$env = 'recettes'
     include dokuwiki
-    include dokuwikirecettes
+    #include dokuwiki_deploy
+    
+    deploy_dokuwiki { "recettes.wiki":
+        env => "recettes.wiki",
+    }
+
+    deploy_dokuwiki { "tajineworld.com":
+        env => "tajineworld.com",
+    }
 }
 
 node 'server1' {
+    #$env = 'politique'
     include dokuwiki
-    include dokuwikipolitique
+    #include dokuwiki_deploy
+    deploy_dokuwiki { "politique.wiki":
+        env => "politique.wiki",
+    }
 }
